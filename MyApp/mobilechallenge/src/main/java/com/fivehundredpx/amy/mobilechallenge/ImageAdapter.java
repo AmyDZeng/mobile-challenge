8488318625cd0a85af93413940e66d7e65162eb8
Amy Zeng <Amy Zeng>
2017-02-06 00:01:18 -0500

    Created scrollable gridview and adapter with temp content

---
+++ MyApp/mobilechallenge/src/main/java/com/fivehundredpx/amy/mobilechallenge/ImageAdapter.java
@@ -0,0 +1,57 @@
+package com.fivehundredpx.amy.mobilechallenge;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    private int mScreenWidth, mScreenHeight;
+    // TODO: tmp, replace with real value
+    private int numPhotos = 20;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+
+        // Grab dimensions
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(displayMetrics);
+        mScreenWidth = displayMetrics.widthPixels;
+        mScreenHeight = displayMetrics.heightPixels;
+    }
+
+    public int getCount() {
+        return numPhotos;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(mScreenWidth/3, mScreenWidth/3));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(R.drawable.nick_cage_tn);
+        return imageView;
+    }
+}
---
+++ MyApp/mobilechallenge/src/main/java/com/fivehundredpx/amy/mobilechallenge/MainActivity.java
@@ -0,0 +1,35 @@
+package com.fivehundredpx.amy.mobilechallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                // TODO: Full screen
+            }
+        });
+    }
+}
+
+/* Checklist:
+ * OAuth?
+ * REST API
+ * actual app
+ */
