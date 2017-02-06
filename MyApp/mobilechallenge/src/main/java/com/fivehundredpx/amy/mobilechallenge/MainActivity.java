5d1d3d7786f4bab7cbcbc9feaf13b4e59b393eaf
Amy Zeng <Amy Zeng>
2017-02-06 01:02:36 -0500

    Added support for REST api calls to 500px api and functions to support grabbing photo json info

--- MyApp/mobilechallenge/src/main/java/com/fivehundredpx/amy/mobilechallenge/MainActivity.java
+++ MyApp/mobilechallenge/src/main/java/com/fivehundredpx/amy/mobilechallenge/MainActivity.java
@@ -6,9 +6,20 @@ import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String DEBUG_JSON = "DEBUG_JSON/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
 
+        getPopularPhotos();
+
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
@@ -26,10 +39,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void getPopularPhotos() /* throws JSONException */ {
+
+        RequestParams params = new RequestParams();
+        params.put("consumer_key", "3xpH2xqdDBbJFSAjVuQCIma2RvOyWFOusJvY61RW");
+        params.put("feature", "popular");
+        params.put("sort", "created_at");
+        params.put("image_size", "3");
+        params.put("include_store", "store_download");
+        params.put("include_states", "voted");
+
+        RestClient.get("/v1/photos", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                try {
+                    JSONArray photos = response.getJSONArray("photos");
+                } catch (JSONException e) {
+                    System.out.println(DEBUG_JSON + e);
+                }
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray photos) {
+                // Won't be called
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, java.lang.String responseString, java.lang.Throwable throwable) {
+                System.out.println(DEBUG_JSON + statusCode);
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, java.lang.Throwable throwable, JSONObject response) {
+                System.out.println(DEBUG_JSON + statusCode);
+            }
+        });
+    }
 }
 
 /* Checklist:
- * OAuth?
- * REST API
- * actual app
+ * Populate adapter with actual pics
+ * request for popular photos: "GET /v1/photos?consumer_key=3xpH2xqdDBbJFSAjVuQCIma2RvOyWFOusJvY61RW&feature=popular&sort=created_at&image_size=3&include_store=store_download&include_states=voted HTTP/1.1"
  */
